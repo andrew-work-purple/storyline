bef5c9f6096c26421158b3226b259d20329e052f
Andrew Palmer <andrew.p@purple.com>
2020-05-19 11:36:07 -0600

    Added event listeners

--- storyline.js
+++ storyline.js
@@ -2,7 +2,7 @@ window.ver = '1.1'
 console.log('storyline functions version ' + window.ver);
 
 var customCSS = `
-    .cs-custom-theme {
+    .cs-base, body, .cs-custom-theme {
         background: #43268F !important;
     }
 `
@@ -10,3 +10,33 @@ var style = document.createElement('style');
 style.type = 'text/css';
 document.head.appendChild(style);
 style.appendChild(document.createTextNode(customCSS));
+
+const controller = {
+    slideType = '',
+    slideProps = {},
+    updateSlide(props) {
+        this.slideType = props.slideType;
+        this.slideProps = props.slideProps;
+    },
+    findSlideFunction() {
+        switch(this.slideType) {
+            case 'simclick': 
+                this.simClick();
+        }
+    },
+    simClick: {
+        clickables: [],
+        background: {},
+
+    },
+    simClick() {
+
+    }
+}
+const eventController = {
+    clickListener(event) {
+        console.log(event);
+    }
+}
+//add listeners
+document.addEventListener('click',controller.clickListener(event));
